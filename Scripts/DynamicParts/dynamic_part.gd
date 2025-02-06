b3f59d8a32d83d09391936f08073a6b7337cebb8
p1petto <ksyuha3803@mail.ru>
2025-02-06 18:42:54 +0800

    голова и тело

--- Scripts/DynamicParts/dynamic_part.gd
+++ Scripts/DynamicParts/dynamic_part.gd
@@ -4,9 +4,13 @@ extends Node2D
 
 @onready var polygon2d = $Polygon2D
 @onready var line2d = $Line2D
+@onready var glare = $Polygon2D/glare
 
 func _ready() -> void:
 	if dynamic_part:
 		polygon2d.polygon = dynamic_part.array_points
 		line2d.points = dynamic_part.array_points
+		
+		if dynamic_part.glare_array_points:
+			glare.polygon = dynamic_part.glare_array_points
 		
--- Scripts/DynamicParts/resource_dynamic_part.gd
+++ Scripts/DynamicParts/resource_dynamic_part.gd
@@ -2,20 +2,6 @@ extends Resource
 
 class_name  DynamicBodyPart
 
-@export var array_points: PackedVector2Array = [
-	Vector2(0, 0),
-	Vector2(4, 0),
-	Vector2(7, 3),
-	Vector2(7, 7),
-	Vector2(7, 10),
-	Vector2(7, 13),
-	Vector2(2, 17),
-	Vector2(0, 17),
-	Vector2(-2, 17),
-	Vector2(-7, 13),
-	Vector2(-7, 10),
-	Vector2(-7, 7),
-	Vector2(-7, 3),
-	Vector2(-4, 0),
-	Vector2(0, 0)
-]
+@export var array_points: PackedVector2Array
+
+@export var glare_array_points: PackedVector2Array
