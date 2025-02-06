efdc6576326c6e85142c601329fc4d76749021e9
p1petto <ksyuha3803@mail.ru>
2025-02-06 22:06:53 +0800

    ноги

--- Scripts/ConditionallyDynamicPart/conditionally_dynamic_part.gd
+++ Scripts/ConditionallyDynamicPart/conditionally_dynamic_part.gd
@@ -4,12 +4,17 @@ extends Node2D
 
 @export var sprite_frames: SpriteFrames
 @export var flip_x = false
+@export var cur_frame:int
+@export var cur_animation: String
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if sprite_frames:
 		animated_sprite.sprite_frames = sprite_frames
 	if flip_x:
 		animated_sprite.flip_h = true
+	if cur_animation:
+		animated_sprite.animation = cur_animation
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
