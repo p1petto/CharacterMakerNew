fda93e9c9d598054c72514b7dad1017ebcd91a8c
p1petto <ksyuha3803@mail.ru>
2025-02-10 20:24:47 +0800

    градиент на голове

--- Scripts/DynamicParts/dynamic_part.gd
+++ Scripts/DynamicParts/dynamic_part.gd
@@ -3,14 +3,18 @@ extends Node2D
 @export var dynamic_part: DynamicBodyPart
 
 @onready var polygon2d = $Polygon2D
+@onready var testpolygon2d = $Polygon2D
 @onready var line2d = $Line2D
 @onready var glare = $Polygon2D/glare
 
+
 func _ready() -> void:
 	if dynamic_part:
-		polygon2d.polygon = dynamic_part.array_points
-		line2d.points = dynamic_part.array_points
-		
-		if dynamic_part.glare_array_points:
-			glare.polygon = dynamic_part.glare_array_points
-		
+		setup_polygon()
+
+func setup_polygon() -> void:
+	polygon2d.polygon = dynamic_part.array_points
+	line2d.points = dynamic_part.array_points
+	
+	if dynamic_part.glare_array_points:
+		glare.polygon = dynamic_part.glare_array_points
