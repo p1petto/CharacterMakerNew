48f58068461fec2f16a66b1052cb0e731843968b
p1petto <ksyuha3803@mail.ru>
2025-02-12 21:57:18 +0800

    заготовка под интерфейс

--- Scripts/maker.gd
+++ Scripts/maker.gd
@@ -1,12 +1,16 @@
 extends Node2D
 
 @onready var subviewport = $SubViewportContainer/SubViewport
+@onready var character_show = $Camera2D/CharacterShow
+#@onready var UI = $Camera2D/UI
+@onready var camera = $Camera2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
+	#UI.size = get_viewport_rect().size
+	#UI.global_position.x = camera.global_position.x - (get_viewport_rect().size.x / 2)
+	#UI.global_position.y = camera.global_position.y + get_viewport_rect().size.y - UI.size.y
+	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
