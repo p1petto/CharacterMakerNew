947a34d44e519b303f90f665a043533c1aec3f92
p1petto <ksyuha3803@mail.ru>
2025-02-14 14:32:51 +0800

    Размещение маркеров динамических элементов

--- Scripts/DynamicParts/dynamic_part.gd
+++ Scripts/DynamicParts/dynamic_part.gd
@@ -2,15 +2,24 @@ extends Node2D
 
 @export var dynamic_part: DynamicBodyPart
 
+
 @onready var polygon2d = $Polygon2D
 @onready var testpolygon2d = $Polygon2D
 @onready var line2d = $Line2D
 @onready var glare = $Polygon2D/glare
+@onready var marker_container = $"../../../../UI/MarkerContainer"
 
+var marker_scene = preload("res://Scenes/marker.tscn")
 
 func _ready() -> void:
 	if dynamic_part:
 		setup_polygon()
+		
+	for m in dynamic_part.markers:
+		var marker = marker_scene.instantiate()
+		marker.position = dynamic_part.array_points[m]
+		marker_container.add_child(marker)
+		#TODO дописать правильное позиционирование
 
 func setup_polygon() -> void:
 	polygon2d.polygon = dynamic_part.array_points
--- Scripts/DynamicParts/resource_dynamic_part.gd
+++ Scripts/DynamicParts/resource_dynamic_part.gd
@@ -5,3 +5,5 @@ class_name  DynamicBodyPart
 @export var array_points: PackedVector2Array
 
 @export var glare_array_points: PackedVector2Array
+
+@export var markers: Array[int]
