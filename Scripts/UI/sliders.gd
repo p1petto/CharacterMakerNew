f00c1aed1187f2ebfb0dda5dfdc433566ac8ad9f
p1petto <ksyuha3803@mail.ru>
2025-02-15 13:38:06 +0800

    Слайдеры заносятся в контейнер, позиция контейнера по y равна позиции по y первого маркера элемента групп

--- Scripts/UI/sliders.gd
+++ Scripts/UI/sliders.gd
@@ -2,20 +2,37 @@ extends Container
 
 @onready var catallog = $"../Catalog"
 @onready var character = $"../../SubViewportContainer/SubViewport/Character"
-
 var slider_scene = preload("res://Scenes/custom_h_slider.tscn")
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for part in character.get_children():
 		if part.is_in_group("Dynamic"):
-			for marker in part.dynamic_part.markers:
-				var slider = slider_scene.instantiate()
+			if part.dynamic_part.markers:
+				var part_container = VBoxContainer.new()
+				part_container.name = part.name
+				
+				# Get the position of the first marker
 				var polygon_2d = part.get_node("Polygon2D")
-				slider.position.y = slider.position.y + polygon_2d.polygon[marker].y * 5 + part.position.y * 5
-				add_child(slider)
+				var first_marker = part.dynamic_part.markers[0]
+				part_container.position.y = polygon_2d.polygon[first_marker].y * 5 + part.position.y * 5
+				
+				add_child(part_container)
+				
+				for marker in part.dynamic_part.markers:
+					var slider = slider_scene.instantiate()
+					part_container.add_child(slider)
 
+	# Get current tab's name from catalog's child
+	var current_part_name = catallog.get_child(catallog.current_tab).name
+	
+	# Hide all containers except the one matching current tab's name
+	for container in get_children():
+		container.visible = (container.name == current_part_name)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_catalog_tab_changed(tab: int) -> void:
+	var current_part_name = catallog.get_child(tab).name
+	for container in get_children():
+		container.visible = (container.name == current_part_name)
