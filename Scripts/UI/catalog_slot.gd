de0f92a02d691c2c0cdcea217988a4b10728431f
p1petto <ksyuha3803@mail.ru>
2025-02-17 21:50:27 +0800

    В каталог добавлены ноги

--- Scripts/UI/catalog_slot.gd
+++ Scripts/UI/catalog_slot.gd
@@ -7,7 +7,8 @@ extends TextureButton
 signal slot_pressed
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	texture_rect.texture = item.icon
+	if item.icon:
+		texture_rect.texture = item.icon
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
