e62e8af9fa6ea2436d8b4018af5ab2463cd18e4b
p1petto <ksyuha3803@mail.ru>
2025-02-18 21:14:08 +0800

    Изменение динамических элементов через слайдеры

--- Scripts/DynamicParts/dynamic_part.gd
+++ Scripts/DynamicParts/dynamic_part.gd
@@ -9,6 +9,7 @@ extends Node2D
 @onready var glare = $Polygon2D/glare
 
 #var marker_scene = preload("res://Scenes/marker.tscn")
+var central_bottom_point: int 
 
 func _ready() -> void:
 	if dynamic_part:
@@ -27,6 +28,8 @@ func setup_polygon(dir) -> void:
 		
 		position.x = dynamic_part.position_x_down
 		z_index = dynamic_part.z_down
+		
+		central_bottom_point = len(dynamic_part.down_array_points) / 2
 	if dir == "top":
 		polygon2d.polygon = dynamic_part.top_array_points
 		line2d.points = dynamic_part.top_array_points
@@ -34,6 +37,8 @@ func setup_polygon(dir) -> void:
 		
 		position.x = dynamic_part.position_x_top
 		z_index = dynamic_part.z_top
+		
+		central_bottom_point = len(dynamic_part.top_array_points) / 2
 	if dir == "right":
 		polygon2d.polygon = dynamic_part.horizontal_array_points
 		line2d.points = dynamic_part.horizontal_array_points
@@ -41,6 +46,8 @@ func setup_polygon(dir) -> void:
 		
 		position.x = dynamic_part.position_x_right
 		z_index = dynamic_part.z_right
+		
+		central_bottom_point = len(dynamic_part.horizontal_array_points) / 2
 	if dir == "left":
 		var mirrored_points = []
 		var mirrored_glare_points = []
@@ -59,3 +66,5 @@ func setup_polygon(dir) -> void:
 		
 		position.x = dynamic_part.position_x_left
 		z_index = dynamic_part.z_left
+		
+		central_bottom_point = len(dynamic_part.horizontal_array_points) / 2
--- Scripts/UI/sliders.gd
+++ Scripts/UI/sliders.gd
@@ -43,8 +43,33 @@ func update_containers_visibility(direction: String) -> void:
 
 		container.visible = is_correct_tab and is_correct_direction
 
-func _on_value_slider_changed(val, m, character_part, axis):
-	pass
+func _on_value_slider_changed(val, m, character_part_name, axis):
+	var character_part = character.get_node(character_part_name)
+	var cur_dir = character.cur_dir
+	if character_part.is_in_group("Dynamic"):
+		
+		if cur_dir == "down":
+			var mirror_x = get_mirror_x(len(character_part.dynamic_part.down_array_points), m)
+			character_part.polygon2d.polygon[m].x = character_part.dynamic_part.down_array_points[m].x - val
+			character_part.line2d.points[m].x = character_part.dynamic_part.down_array_points[m].x - val
+			character_part.polygon2d.polygon[mirror_x].x = character_part.dynamic_part.down_array_points[mirror_x].x + val
+			character_part.line2d.points[mirror_x].x = character_part.dynamic_part.down_array_points[mirror_x].x + val
+		elif cur_dir == "top":
+			var mirror_x = get_mirror_x(len(character_part.dynamic_part.top_array_points), m)
+			character_part.polygon2d.polygon[m].x = character_part.dynamic_part.top_array_points[m].x + val
+			character_part.line2d.points[m].x = character_part.dynamic_part.top_array_points[m].x + val
+		elif cur_dir == "right":
+			var mirror_x = get_mirror_x(len(character_part.dynamic_part.horizontal_array_points), m)
+			character_part.polygon2d.polygon[m].x = character_part.dynamic_part.horizontal_array_points[m].x + val
+			character_part.line2d.points[m].x = character_part.dynamic_part.horizontal_array_points[m].x + val
+		pass
+	elif character_part.is_in_group("ConditionallyDynamic"):
+		pass
+		
+func get_mirror_x(count_of_points, marker):
+	if count_of_points % 2 == 0:
+		return count_of_points - marker
+	return count_of_points - marker - 1
 
 func update_sliders(character_part_name):
 	var character_part = character.get_node(character_part_name)
