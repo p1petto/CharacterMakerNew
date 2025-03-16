0de78a0e4c86025e24310e8df1f542ecce7adba0
p1petto <ksyuha3803@mail.ru>
2025-03-16 13:51:21 +0800

    added: Смена цвета одежды

--- Scenes/color_picker_button_container.gd
+++ Scenes/color_picker_button_container.gd
@@ -21,15 +21,16 @@ func _create_and_assign_color_picker(child) -> void:
 	color_picker_button.visible = false
 	
 	var character_element
-	if child.is_in_group("DynamicClothesTab"):
+	if child.is_in_group("DynamicClothesTab") or child.is_in_group("ClothesTab"):
 		var target_node = child.catalog_items[0].item_class
 		target_node = character.find_child(target_node, true, false)
-		character_element = target_node.find_child("DynamicClothes", true, false)
+		if child.is_in_group("DynamicClothesTab"):
+			character_element = target_node.find_child("DynamicClothes", true, false)
+		else:
+			character_element = target_node.find_child("Clothes", true, false)
 		character_element.color_picker_button = color_picker_button
 		character_element._connect_color_changed_signal()
 		character_element._connect_color()
-	elif child.is_in_group("ClothesTab"):
-		print ()
 	else:
 		character_element = character.find_child(child.name, true, false)
 		character_element.color_picker_button = color_picker_button
--- Scripts/clothes.gd
+++ Scripts/clothes.gd
@@ -1,9 +1,13 @@
 extends TextureRect
 var resource_clothes: Clothes
+var color_picker_button
+var cur_color: Color = Color(1, 1, 1)
+var start_color: Color = Color(1, 1, 1)
 
 func initialize():
 	texture = resource_clothes.clothes_texture
 	change_dir()
+	set_start_color()
 
 func change_dir():
 	
@@ -30,3 +34,22 @@ func change_dir():
 				elif Global.current_animation == "walk":
 					position = resource_clothes.left_walk_position
 		
+		
+func _connect_color_changed_signal():
+	color_picker_button.color_changed.connect(_on_color_changed)
+	
+func _connect_color():
+	color_picker_button._on_color_picker_color_changed(cur_color)
+	color_picker_button.color_picker.color = cur_color
+	set_start_color()
+	
+func set_start_color() -> void:
+	_on_color_changed(start_color)
+	color_picker_button.color_picker.color = start_color
+	print(start_color)
+	
+func _on_color_changed(new_color: Color) -> void:
+	cur_color = new_color
+	self_modulate = cur_color
+	color_picker_button.set_new_bg_color(new_color)
+	
