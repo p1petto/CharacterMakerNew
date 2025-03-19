03c20809655ebafb1ab688816009b78bd1710a8f
p1petto <ksyuha3803@mail.ru>
2025-03-19 17:59:12 +0800

    added: вызов окна для сохранения спрайтщита

--- Scripts/UI/export.gd
+++ Scripts/UI/export.gd
@@ -1,45 +1,89 @@
 extends Button
-
 @onready var animation_player_button = $"../AnimationPlayer"
 @onready var dir_down_button = $"../../DirectionButtons/Down"
 @onready var character = $"../../../SubViewportContainer/SubViewport/Character"
 @onready var animation_controller = $"../../../AnimationController"
 @onready var subviewport = $"../../../SubViewportContainer/SubViewport"
 
+var final_sprite_sheet: Image
 
 func _on_button_up() -> void:
+	generate_sprite_sheet()
+
+func generate_sprite_sheet() -> void:
 	animation_controller.clear_all_states()
 	var image_size = subviewport.get_texture().get_size()
 	var rows = len(Global.directions)
-
+	
+	var animation_widths = []
+	var animation_heights = []
+	var animation_sprite_sheets = []
+	
 	for i in len(Global.animation_states):
 		Global.current_animation = Global.animation_states[i]
 		animation_controller.change_animation()
-
 		var cols = len(character.get_child(0).get(animation_controller.get_offset_name()))
-		var total_width = image_size.x * cols
-		var total_height = image_size.y * rows
-		var sprite_sheet = Image.create(total_width, total_height, false, Image.FORMAT_RGBA8)
-
+		var anim_width = image_size.x * cols
+		var anim_height = image_size.y * rows
+		
+		animation_widths.append(anim_width)
+		animation_heights.append(anim_height)
+		
+		var sprite_sheet = Image.create(anim_width, anim_height, false, Image.FORMAT_RGBA8)
+		
 		for j in len(Global.directions):
 			Global.current_dir = Global.directions[j]
-
 			character.on_direction_changed()
 			character.change_dir_for_parts()
-
 			var cur_offset_animation = character.get_child(0).get(animation_controller.get_offset_name())
+			
 			for k in len(cur_offset_animation):
 				animation_controller.animation_frame = k
 				animation_controller.animate_children()
-
 				await get_tree().process_frame
-
+				
 				var screen = subviewport.get_texture().get_image()
 				var x_offset = k * image_size.x
 				var y_offset = j * image_size.y
+				
 				sprite_sheet.blit_rect(screen, Rect2(Vector2.ZERO, image_size), Vector2(x_offset, y_offset))
-
-		var file_name = "sprite_sheet_" + Global.current_animation + ".png"
-		sprite_sheet.save_png(file_name)
-
+		
+		animation_sprite_sheets.append(sprite_sheet)
+	
+	var max_width = 0
+	for width in animation_widths:
+		max_width = max(max_width, width)
+	
+	var total_height = 0
+	for height in animation_heights:
+		total_height += height
+	
+	final_sprite_sheet = Image.create(max_width, total_height, false, Image.FORMAT_RGBA8)
+	
+	var current_y_offset = 0
+	for i in len(animation_sprite_sheets):
+		final_sprite_sheet.blit_rect(animation_sprite_sheets[i], 
+			Rect2(0, 0, animation_widths[i], animation_heights[i]), 
+			Vector2(0, current_y_offset))
+		current_y_offset += animation_heights[i]
+	
+	show_save_dialog()
+	
 	animation_controller.clear_all_states()
+
+func show_save_dialog() -> void:
+	var file_dialog = FileDialog.new()
+	file_dialog.access = FileDialog.ACCESS_FILESYSTEM
+	file_dialog.file_mode = FileDialog.FILE_MODE_SAVE_FILE
+	file_dialog.add_filter("*.png ; PNG Images")
+	file_dialog.title = "Сохранить спрайт-лист"
+	file_dialog.current_path = "sprite_sheet.png"
+	
+	file_dialog.connect("file_selected", Callable(self, "_on_file_selected"))
+	
+	get_tree().root.add_child(file_dialog)
+	file_dialog.popup_centered_ratio(0.7)
+
+func _on_file_selected(path: String) -> void:
+	final_sprite_sheet.save_png(path)
+	print("Спрайт-лист сохранен: " + path)
